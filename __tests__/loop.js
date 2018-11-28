4ea7c37b20994768721d9b13fbb6954e2e5feed1
Eugeny Burnashov <shamcode@gmail.com>
2018-11-28 20:13:33 +0700

    Migrate loop tests to jest

--- __tests__/loop.js
+++ __tests__/loop.js
@@ -1,5 +1,24 @@
 import { compile, renderWidget } from './helpers';
 
+beforeEach( () => {
+    window.MyLi = compile`
+        <li>{{ props.id + ':' + props.value }}</li>
+    `;
+    window.MyUl = compile`
+        <ul>
+            {% for element of list %}
+                <MyLi props="{{ element }}"/>
+            {% endfor %}
+        </ul>
+    `;
+} );
+
+afterEach( () => {
+    delete window.MyLi;
+    delete window.MyUl;
+} );
+
+
 it( 'should render arrays', async() => {
     expect.assertions( 3 );
     const { html, widget } = await renderWidget(
@@ -71,4 +90,96 @@ it( 'should iterate over objects', async() => {
         }
     } );
     expect( widget.container.innerHTML ).toBe( '<div>a: 1; c: 3; d: 4; </div>' );
+} );
+
+it( 'should iterate over arrays without options', async() => {
+    expect.assertions( 1 );
+    const { html } = await renderWidget(
+        compile`
+            <div>
+                {% for obj %}
+                    {{ name }};
+                {% endfor %}
+            </div>
+        `,
+        {
+            obj: [
+                { name: 'a' },
+                { name: 'b' },
+                { name: 'c' }
+            ]
+        }
+    );
+    expect( html ).toBe( '<div>a; b; c; </div>' );
+} );
+
+it( 'should delete old items from childred map with custom tag', async() => {
+    expect.assertions( 2 );
+    const { html, widget } = await renderWidget(
+        compile`
+            <div>
+                <MyUl list="{{ list }}"/>
+            </div>
+        `,
+        {
+            list: [
+                {
+                    id: 1,
+                    value: 'a'
+                },
+                {
+                    id: 2,
+                    value: 'b'
+                },
+                {
+                    id: 3,
+                    value: 'c'
+                }
+            ]
+        }
+    );
+    expect( html ).toBe(
+        '<div><ul><li>1:a</li><!--MyLi--><li>2:b</li><!--MyLi--><li>3:c</li><!--MyLi--></ul></div>'
+    );
+
+    widget.update( {
+        list: [
+            {
+                id: 1,
+                value: 'a'
+            },
+            {
+                id: 3,
+                value: 'c'
+            }
+        ]
+    } );
+    expect( widget.container.innerHTML ).toBe(
+        '<div><ul><li>1:a</li><!--MyLi--><li>3:c</li><!--MyLi--></ul></div>'
+    );
+} );
+
+it( 'should not expose local variables', async() => {
+    expect.assertions( 1 );
+    const { html } = await renderWidget(
+        compile`
+            <section>
+                {% for a of as %}
+                    <i>
+                        {% for b of bs %}
+                            <b>{{ b }}</b>
+                        {% endfor %}
+                    </i>
+                {% endfor %}
+            </section>
+        `,
+        {
+            as: [ 'a', 'b' ],
+            bs: [ 1, 2 ],
+            b: 'GLOBAL'
+        }
+    );
+    expect( html ).toBe(
+        '<section><i><b>1</b><b>2</b></i><i><b>1</b><b>2</b></i></section>'
+    );
 } );
